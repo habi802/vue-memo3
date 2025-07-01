3819856b9769f8f444e07188d14086e345d7d465
habi802 <shb8002@gmail.com>
2025-07-01 14:57:59 +0900

    [feat] home 화면 추가

---
+++ src/components/MemoCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-link class="item">
+    <div class="d-flex pt-3">
+      <div class="pb-3 mb-0 w-100">
+        <div class="d-flex justify-content-between">
+          <b>제목</b>
+          <div>
+            <span role="button">삭제</span>
+          </div>
+        </div>
+        <div class="mt-2">날짜</div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+  .item {
+    background-color: #f8f9fa;
+    border: 1px solid #eee;
+    display: block;
+    color: #000;
+    text-decoration: none;
+    padding: 20px 30px;
+    margin: 15px 0;
+
+    &:hover {
+      border-color: #aaa;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <script setup>
-
+  import MemoCard from '@/components/MemoCard.vue';
 </script>
 
 <template>
+  <div class="memo-list">
+    <router-link class="add btn btn-light">
+      등록하기
+    </router-link>
 
+    <MemoCard />
+    <MemoCard />
+    <MemoCard />
+  </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .memo-list {
+    .add {
+      display: block;
+      padding: 25px;
+      border: 1px solid #eee;
+    }
+  }
 </style>
