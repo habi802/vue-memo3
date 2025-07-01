6d154c2e1fbd475b8815ca695109bd2dc7a24664
habi802 <shb8002@gmail.com>
2025-07-01 15:40:38 +0900

    [feat] 글 등록 추가

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:8080/api';
 
 class HttpService {
+  async post(item) {
+    const res = await axios.post('/memo', item);
+    return res.data;
+  }
+  
   async get() {
     const res = await axios.get('/memo');
     return res.data;
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,9 @@
 <script setup>
-  import { reactive } from 'vue';
+  import { reactive, useTemplateRef } from 'vue';
+  import httpService from '@/services/HttpService';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
 
   const state = reactive({
     memo: {
@@ -10,22 +14,43 @@
     }
   });
 
-  const submit = () => {
+  const refTitle = useTemplateRef('ref_title');
+  const refContent = useTemplateRef('ref_content');
+
+  const submit = async () => {
     if (state.memo.title.trim().length === 0) {
       alert('제목을 작성해 주십시다.');
+      refTitle.value.focus();
       return;
     }
     if (state.memo.title.trim().length > 30) {
       alert('제목은 30자 내로 작성해 주십시다.');
+      refTitle.value.focus();
       return;
     }
     if (state.memo.content.trim().length === 0) {
       alert('내용을 작성해 주십시다.');
+      refContent.value.focus();
       return;
     }
     if (state.memo.content.trim().length > 200) {
       alert('내용은 200자 이내로 작성해 주십시다.');
+      refContent.value.focus();
       return;
+    }
+
+    let data = null;
+    const bodyJson = {
+      title: state.memo.title,
+      content: state.memo.content
+    };
+
+    data = await httpService.post(bodyJson);
+
+    if (data.resultData === 1) {
+      router.push({ path: '/' });
+    } else {
+      alert(data.resultMessage);
     }
   };
 </script>
@@ -34,11 +59,11 @@
   <form class="detail" @submit.prevent="submit">
     <div class="mb-3">
       <label for="title" class="form-label">제목</label>
-      <input type="text" id="title" class="form-control p-3" v-model="state.memo.title">
+      <input type="text" id="title" ref="ref_title" class="form-control p-3" v-model="state.memo.title">
     </div>
     <div class="mb-3">
       <label for="content" class="form-label">내용</label>
-      <textarea type="text" id="content" class="form-control p-3" v-model="state.memo.content"></textarea>
+      <textarea type="text" id="content" ref="ref_content" class="form-control p-3" v-model="state.memo.content"></textarea>
     </div>
     <button type="submit" class="btn btn-primary w-100 py-3">저장</button>
   </form>
