3332701b08cdc999b7d23c53016e754efe5e1fa6
habi802 <shb8002@gmail.com>
2025-07-03 17:48:25 +0900

    [feat] 더 보기 기능 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,71 @@
 <script setup>
-  import { reactive, onMounted } from 'vue';
+  import { ref, onMounted, watch } from 'vue';
   import httpService from '@/services/HttpService';
   import MemoCard from '@/components/MemoCard.vue';
 
-  const state = reactive({
-    memos: []
-  });
+  const memos = ref([]);
 
   onMounted(() => {
-    getItems({});
+    getItems({ limit: limit + 1 });
   });
 
+  // 더 보기를 위한 변수
+  let lastIdx = 0;
+  let isMemoEnd = ref(false);
+  let limit = 5;
+
+  // 목록 가져오기
   const getItems = async params => {
     const data = await httpService.get(params);
-    state.memos = data.resultData;
+    const newMemos = data.resultData;
+
+    if (newMemos.length <= limit) {
+      isMemoEnd.value = true;
+    }
+
+    memos.value.push(...newMemos.slice(0, limit));
+
+    if (newMemos.length > 0) {
+      lastIdx = newMemos[newMemos.length - 2]?.memoId ?? 0;
+    }
   };
 
-  const model = {
-    searchOption: '',
-    searchText: ''
-  };
+  // 검색을 위한 변수
+  const searchOption = ref('');
+  const previousOption = ref('');
+  const searchText = ref('');
+  const previousText = ref('');
 
-  const search = async () => {
-    const params = {
-      search_option: model.searchOption,
-      search_text: model.searchText
-    };
-    getItems(params);
+  let searchParams = null;
+
+  // 검색
+  const search = () => {
+    if (searchOption.value !== previousOption.value || searchText.value !== previousText.value) {
+      lastIdx = 0;
+      memos.value = [];
+      isMemoEnd.value = false;
+    }
+
+    searchParams = {
+      search_option: searchOption.value,
+      search_text: searchText.value,
+      limit: limit + 1
+    }
+    getItems(searchParams);
   }
 
+  // 더 보기
+  const loadMore = () => {
+    searchParams = {
+      search_option: searchOption.value,
+      search_text: searchText.value,
+      last_idx: lastIdx,
+      limit: limit + 1
+    };
+    getItems(searchParams);
+  };
+
+  // 삭제
   const deleteItem = async memoId => {
     if (!confirm('삭제할까요?')) {
       return;
@@ -38,7 +75,7 @@
     const data = await httpService.delete(params);
 
     if (data.resultData === 1) {
-      const deleteIdx = state.memos.findIndex(item => item.memoId === memoId);
+      const deleteIdx = memos.findIndex(item => item.memoId === memoId);
       if (deleteIdx >= 0) {
         state.memos.splice(deleteIdx, 1);
       }
@@ -53,18 +90,20 @@
     </router-link>
 
     <div class="d-flex mb-3 mt-3">
-      <select class="form-control me-3 w-25" v-model="model.searchOption">
+      <select class="form-control me-3 w-25" v-model="searchOption">
         <option value="">제목+내용</option>
         <option value="title">제목</option>
         <option value="content">내용</option>
       </select>
-      <input type="text" class="form-control me-3 w-50" v-model="model.searchText">
+      <input type="text" class="form-control me-3 w-50" v-model="searchText">
       <button class="btn btn-primary w-25" @click="search">검색</button>
     </div>
 
-    <MemoCard v-for="m in state.memos" :item="m" :key="m.id" @delete-item="deleteItem" />
+    <MemoCard v-for="m in memos" :item="m" :key="m.id" @delete-item="deleteItem" />
 
-    <button type="button" class="more btn btn-light w-100">더 보기</button>
+    <button v-if="!isMemoEnd" type="button" class="more btn btn-light w-100" @click="loadMore">
+      더 보기
+    </button>
   </div>
 </template>
 
